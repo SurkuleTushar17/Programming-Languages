20b97afc4646b9599791a3eb810e81d557721231
Dev_Tushar17 <surkuletushar17@gmail.com>
2026-09-24 19:24:30 +0530

    Create DML.sql
    Signed-off-by: Dev_Tushar17 <surkuletushar17@gmail.com>

---
+++ SQL/DML.sql
@@ -0,0 +1,9 @@
+INSERT INTO Seen VALUES (1, 'One Piece', "GOAT");
+
+INSERT INTO Seen VALUES (2, 'Naruto', "GOAT");
+
+UPDATE Seen SET review = "Awasome" WHERE id = 2;
+
+DELETE FROM students WHERE id = 2;
+
+SELECT * FROM Seen;
