6f2958dbc40d334eb14c9e3673914b88990efd5a
Dev_Tushar17 <surkuletushar17@gmail.com>
2026-09-25 07:39:56 +0530

    Create ASCII.c
    Signed-off-by: Dev_Tushar17 <surkuletushar17@gmail.com>

---
+++ C Language/ASCII.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+
+char ch = 65; // For number to chracter or digit
+
+printf("ASCII value is %c.\n", ch);
+// ASCII = American Standard Code of Information Interchange
+
+char di = 'N'; // For Character or digit to number
+printf("ASCII number is %d\n", di);
+/*
+0 to 9 = 47 to 58
+A to Z = 65 to 90
+a to z = 97 to 122
+*/
+
+return 0;
+}
